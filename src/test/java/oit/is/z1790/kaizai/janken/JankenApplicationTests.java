05fc21b5b49b6f9b3d36dc97e2fe564d45512c7c
Shibata1313 <e1b21047@oit.ac.jp>
2023-09-26 14:55:15 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/z1790/kaizai/janken/JankenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1790.kaizai.janken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
