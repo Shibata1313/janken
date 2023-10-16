ec4d14925d918791690a8daf96e88414cdb48cbe
Shibata1313 <e1b21047@oit.ac.jp>
2023-10-16 14:46:10 +0900

    エントリーした人の名前の表示

--- src/main/java/oit/is/z1790/kaizai/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1790/kaizai/janken/controller/JankenController.java
@@ -1,5 +1,8 @@
 package oit.is.z1790.kaizai.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z1790.kaizai.janken.model.Janken;
+import oit.is.z1790.kaizai.janken.model.Entry;
 
 @Controller
 public class JankenController {
+
+  @Autowired
+  private Entry entry;
+
   @PostMapping("/janken")
   public String janken(@RequestParam String userName, ModelMap model) {
     model.addAttribute("userName", userName);
@@ -19,7 +27,11 @@ public class JankenController {
   }
 
   @GetMapping("/janken")
-  public String janken() {
+  public String janken(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
+    model.addAttribute("login_user", loginUser);
     return "janken.html";
   }
 
